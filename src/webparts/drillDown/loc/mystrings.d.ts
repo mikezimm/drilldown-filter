7cc17870ecf0a49497757653fdb04eae9ab9c3b9
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-09-12 10:09:57 -0400

    InitialCommit

---
+++ src/webparts/drillDown/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDrillDownWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DrillDownWebPartStrings' {
+  const strings: IDrillDownWebPartStrings;
+  export = strings;
+}
